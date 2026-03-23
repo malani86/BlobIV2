6fef0b9d27ac130b1807edfc26d87da6c46624f5
malani86 <134076665+malani86@users.noreply.github.com>
2026-03-23 11:06:13 +0100

    Update install_packages.py

--- install_packages.py
+++ install_packages.py
@@ -1,6 +1,6 @@
 import subprocess
 
-packages = ['pyside6', 'scikit-image', 'numpy', 'matplotlib', 'scipy','joblib','tifffile','imagecodecs','torch','opencv-python']
+packages = ['pyside6', 'scikit-image', 'numpy', 'matplotlib', 'scipy','joblib','tifffile','imagecodecs','torch','opencv-python','imageio']
 
 for package in packages:
     subprocess.check_call(['pip', 'install', package])
